f7511e74b76c332d4984b69020b1f5907087b294
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2022-12-12 22:31:17 +0300

    Automatic_telegram_typer

---
+++ main.py
@@ -0,0 +1,8 @@
+import pyautogui
+import time
+time.sleep(4)
+count=0
+while count <=1000:
+    pyautogui.typewrite("Dear DAGIM , You are going to be hacked by a coder, Please be take care ,reply or Contact me.")
+    pyautogui.press("enter")
+    count=count+1
